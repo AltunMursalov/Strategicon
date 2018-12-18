23fdff6c4285e51dd347e386196ac1fd1677fc36
Altun Mursalov <altun.mursalov00@gmail.com>
2018-12-18 23:18:31 +0400

    Init bootstrap 4.x.x included

---
+++ application/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <v-container>
+      <router-view />
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+@import './node_modules/bootstrap/scss/bootstrap.scss';
+</style>
