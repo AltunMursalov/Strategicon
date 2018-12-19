3ae6e4924f99b4538d899e103f8bf0e35b61ea9d
Altun Mursalov <altun.mursalov00@gmail.com>
2018-12-19 14:23:59 +0400

    Header navbar

---
+++ application/src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="#">Главная страница</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" 
+                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Информация о стратегиях
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Классификация жанров стратегий</a>
+                    <a class="dropdown-item" href="#">Описание жанров стратегий</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Форум</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Группа ВКонтакте</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Для разработчиков</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    О нас
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Наша команда</a>
+                    <a class="dropdown-item" href="#">Сотрудничество</a>
+                </div>
+            </li>
+          </ul>
+          <form class="form-inline mt-2 mt-md-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+</template>
--- application/src/router/index.js
+++ application/src/router/index.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 // Pages
-import Home from '@/components/pages/Home';
+import Home from '@/components/pages/Home'
+
+// Global components
+import Header from '@/components/Header'
+
+// Register components
+Vue.component('app-header', Header)
 
 Vue.use(Router)
 
@@ -10,8 +16,11 @@ export default new Router({
   routes: [
     {
       name: 'Home',
-      component: Home,
-      path: '/'
+      path: '/',
+      components: {
+        default: Home,
+        header: Header
+      }
     }
   ]
 })
